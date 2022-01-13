c39fee5283d889487984dcfdf8275c6e950f2321
Ramiro Zavala <ramiroaz@gmail.com>
2022-01-13 13:01:06 -0300

    Project created

---
+++ backend/src/test/java/com/zavala/dsmovie/DsmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zavala.dsmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
