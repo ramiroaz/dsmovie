25cc6ae0313fe523a0152d7300c146d1eaf8e6ba
Ramiro Zavala <ramiroaz@gmail.com>
2022-01-13 23:51:25 -0300

    Routes

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -8,7 +8,7 @@ function Navbar() {
             <nav className="container">
                 <div className="dsmovie-nav-content">
                     <h1>DSMovie</h1>
-                    <a href="https://github.com/ramiroaz">
+                    <a className="dsmovie-href" href="https://github.com/ramiroaz">
                         <div className="dsmovie-contact-container">
                             <GithubIcon />
                             <p className="dsmovie-contact-link">/ramiroaz</p>
